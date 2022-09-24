281bbe7a8453ce2279a8bf3c07cb17025d4b46eb
Tony-Freire <antoniowilliam.nave@gmail.com>
2022-09-24 16:18:00 -0300

    GET PULL finalizado

--- src/controllers/pollController.js
+++ src/controllers/pollController.js
@@ -2,7 +2,7 @@ import {PollSchema} from "../schemas/PoolSchema.js";
 import dayjs from "dayjs";
 import database from "../database/db.js";
 
-export default async function CreatePoll(req, res)
+async function CreatePoll(req, res)
 {
     const {title,expireAt} = req.body;
     
@@ -23,7 +23,7 @@ export default async function CreatePoll(req, res)
                 title,
                 newExepireAt
             })
-            return res.send(newExepireAt);
+            return res.send({title,newExepireAt}).status(201);
          }
          if(!validation.error)
          {
@@ -31,12 +31,22 @@ export default async function CreatePoll(req, res)
                 title,
                 expireAt
             })
-            return res.send({title,expireAt});
+            return res.send({title,expireAt}).status(201);
          }
     }
     catch(error){
         console.error(error);
         return 'error';
     }
-    return res.send(console.log("foi"));
 }  
+
+ async function PollView(req, res){
+    try {
+        const polls = await database.collection('polls').find().toArray();
+        return res.send(polls);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+} 
+export {CreatePoll,PollView}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import express, { json } from "express";
 import cors from "cors";
-import CreatePoll from "./controllers/pollController.js";
+import {CreatePoll,PollView} from "./controllers/pollController.js";
+
 import dotenv from "dotenv";
+
 
 dotenv.config();
 const PORT =process.env.PORT||5000;
@@ -10,5 +12,6 @@ app.use(cors());
 app.use(json());
 
 app.post('/poll',CreatePoll);
+app.get('/poll',PollView);
 
 app.listen(PORT,()=>console.log(`"servidor rodando na porta ${PORT}`));
